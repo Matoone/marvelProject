981fb68fab1238bfa1bf048f94f1b77b2b7f7519
Matthieu Degré <matthieu.degre@gmail.com>
2021-12-31 13:18:06 +0100

    test: fix home test

--- front-end/src/pages/Home.test.tsx
+++ front-end/src/pages/Home.test.tsx
@@ -4,44 +4,52 @@ import { getCharacters } from "../queries";
 import Home from "./Home";
 
 const mockWithData = () =>
-  Array(5).fill({
-    request: {
-      query: getCharacters,
-      variables: {
-        offset: 0,
-        limit: 20,
-        name: "",
-      },
-    },
-
-    result: {
-      data: {
-        characters: {
-          hasMore: false,
-          characters: [
-            {
-              id: "1",
-              name: "Bob",
-              image: {
-                url: "http://www.test.co",
-                extension: "jpg",
-              },
-            },
-          ],
+  Array(5)
+    .fill("")
+    .map((v, i) => ({
+      request: {
+        query: getCharacters,
+        variables: {
+          offset: i,
+          limit: 20,
+          name: "",
         },
       },
-    },
-  });
+
+      result: {
+        data: {
+          characters: {
+            marvelMessage: "hello world",
+            hasMore: false,
+            characters: [
+              {
+                id: "1",
+                name: "Bob",
+                image: {
+                  url: "http://www.test.co",
+                  extension: "jpg",
+                },
+              },
+            ],
+          },
+        },
+      },
+    }));
 
 const mockWithError = {
   request: {
     query: getCharacters,
+    variables: {
+      offset: 0,
+      limit: 20,
+      name: "",
+    },
   },
 
   error: new Error("An error occurred"),
 };
 
-test("Home page loading", () => {
+test("Home page loading", async () => {
   act(() => {
     const component = (
       <MockedProvider mocks={[...mockWithData()]} addTypename={false}>
